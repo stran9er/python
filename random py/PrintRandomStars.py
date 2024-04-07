4b5dfc197de92c9ba0229d2c021c009115d45802
Tarric Ali <stran9er@gmail.com>
2024-04-07 15:18:22 -0500

    Update PrintRandomStars.py

--- random py/PrintRandomStars.py
+++ random py/PrintRandomStars.py
@@ -1,12 +1,24 @@
-# Function to print random stars
+#Function to print random stars
 def Stars(n: int):
     # Variable to hold our stars, initiated as blank space.
     str = ""
 
-    for i in range (n):
+    for i in range(n):
         # Append our string with a star.
         str +="*"
         print(str)
 
-# Call the function and set to 5.
+
+# Call function and set to user feedback. 
+print(f"User input based:\n")
+Stars(int(input("How many stars do you want?")))
+
+# Set a value that the programmer or system assigns
+print("\nSystem based:\n")
 Stars(5)
+
+# Sets a random value from the range.
+print("\nRandom Set:\n")
+import random as rnd
+rnd_range = rnd.randint(0,100+1)
+Stars(rnd_range)
